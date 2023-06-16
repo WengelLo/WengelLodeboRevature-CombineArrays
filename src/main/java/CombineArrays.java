879dd8e885a637e00f4617aaf321eec5adfdf283
Wengel Lodebo <tes.wengel@gmail.com>
2023-06-16 04:28:28 +0000

    l

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,19 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int[] combined = new int[arr1.length + arr2.length];
+        int index = 0;
+        
+        for (int i = 0; i < arr1.length; i++) {
+            combined[index] = arr1[i];
+            index++;
+        }
+        
+        for (int i = 0; i < arr2.length; i++) {
+            combined[index] = arr2[i];
+            index++;
+        }
+        
+        return combined;
     }
 }
